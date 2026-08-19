b38757ae1ea0f064c32c402df083e2dd8901cfbd
josephan-justin <josephanjustin2@gmail.com>
2026-08-19 22:33:28 +0700

    feat(server) : implement dashboard admin & validation

--- server/routers/admin.js
+++ server/routers/admin.js
@@ -1,16 +1,19 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-const AdminController = require('../controllers/adminController')
-const authentication = require('../middlewares/authentication')
-const authorizationAdmin = require('../middlewares/authorizationAdmin')
+const AdminController = require("../controllers/adminController");
+const authentication = require("../middlewares/authentication");
+const authorizationAdmin = require("../middlewares/authorizationAdmin");
 
-router.use(authentication)
-router.use(authorizationAdmin)
+router.use(authentication);
+router.use(authorizationAdmin);
 
-router.get('/auctions', AdminController.getAuctions)
-router.post('/auctions', AdminController.createAuction)
-router.get('/auctions/:id', AdminController.getAuctionById)
-router.put('/auctions/:id', AdminController.updateAuction)
-router.delete('/auctions/:id', AdminController.deleteAuction)
+router.get("/auctions", AdminController.getAuctions);
+router.post("/auctions", AdminController.createAuction);
+router.get("/dashboard", AdminController.getDashboard);
+router.get("/bids", AdminController.getBids);
+router.get("/auctions/:id", AdminController.getAuctionById);
+router.patch("/auctions/:id/status", AdminController.updateAuctionStatus);
+router.put("/auctions/:id", AdminController.updateAuction);
+router.delete("/auctions/:id", AdminController.deleteAuction);
 
-module.exports = router
+module.exports = router;
