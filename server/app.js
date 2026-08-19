0cc81ae78891c600a223391423fb5a88a3cfbf3f
dhaifanazzaky1 <dhaifanazzaky004@gmail.com>
2026-08-19 16:24:19 +0800

    feat fix app skeleton

--- server/app.js
+++ server/app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const cors = require('cors')
+const router = require('./routers');
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(router)
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
