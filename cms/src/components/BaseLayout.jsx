4a03ecd5e36133eddc7e8b8dc06e9b4a5848b1b5
josephan-justin <josephanjustin2@gmail.com>
2026-08-20 15:04:28 +0700

    feat(cms) : add sidebar

--- cms/src/components/BaseLayout.jsx
+++ cms/src/components/BaseLayout.jsx
@@ -1,14 +1,19 @@
 import { Outlet } from "react-router";
-import Navbar from "./Navbar";
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
 
 export default function BaseLayout() {
   return (
-    <>
-      <Navbar />
+    <div className="flex min-h-screen bg-gray-100">
+      <Sidebar />
 
-      <main>
-        <Outlet />
-      </main>
-    </>
+      <div className="flex flex-1 flex-col">
+        <Navbar />
+
+        <main className="flex-1 p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
   );
 }
